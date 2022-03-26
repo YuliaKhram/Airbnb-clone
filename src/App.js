4fc0bebe2261501288cfe3848bf5c395de20a39a
YuliaKhram <khramtsova.y@gmail.com>
2022-03-25 19:42:01 -0700

    Revert "upd'"
    This reverts commit 1ba029c29a5b9dfb81ef677fc5b25f8116db4b1b.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import Header from "./Components/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/search">{/* {SearchPage} */}</Route>
+          <Route path="/">{/* {Home} */}</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
