1ba029c29a5b9dfb81ef677fc5b25f8116db4b1b
YuliaKhram <khramtsova.y@gmail.com>
2022-03-25 19:35:00 -0700

    upd'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Switch } from "react-router";
-import Header from "./Components/Header";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/search">{/* {SearchPage} */}</Route>
-          <Route path="/">{/* {Home} */}</Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
